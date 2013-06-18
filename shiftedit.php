fe5fde2a89ac5711b6d0d838f3a1550d44f9dec7
Adam Jimenez <adam.jimenez@gmail.com>
2013-06-18 19:53:55 +0200

    optional authentication

--- shiftedit.php
+++ shiftedit.php
@@ -1,7 +1,7 @@
 <?php
 //config
-$username = 'admin';
-$password = 'pass';
+$username = '';
+$password = '';
 $dir = dirname(__FILE__).'/../';
 
 //api version
@@ -11,51 +11,54 @@ $version = '1';
 error_reporting(E_ALL ^ E_NOTICE);
 
 //basic authentication
-// function to parse the http auth header
-function http_digest_parse($txt)
-{
-    // protect against missing data
-    $needed_parts = array('nonce'=>1, 'nc'=>1, 'cnonce'=>1, 'qop'=>1, 'username'=>1, 'uri'=>1, 'response'=>1);
-    $data = array();
-    $keys = implode('|', array_keys($needed_parts));
 
-    preg_match_all('@(' . $keys . ')=(?:([\'"])([^\2]+?)\2|([^\s,]+))@', $txt, $matches, PREG_SET_ORDER);
-
-    foreach ($matches as $m) {
-        $data[$m[1]] = $m[3] ? $m[3] : $m[4];
-        unset($needed_parts[$m[1]]);
-    }
-
-    return $needed_parts ? false : $data;
-}
-
-if (empty($_SERVER['PHP_AUTH_DIGEST'])) {
-    header('HTTP/1.1 401 Unauthorized');
-    header('WWW-Authenticate: Digest realm="'.$realm.'",qop="auth",nonce="'.uniqid().'",opaque="'.md5($realm).'"');
-    die('login required');
-}
-
-// analyze the PHP_AUTH_DIGEST variable
-if ( !($data = http_digest_parse($_SERVER['PHP_AUTH_DIGEST'])) || $username !== $data['username'] ){
-    header('HTTP/1.1 401 Unauthorized');
-    header('WWW-Authenticate: Digest realm="'.$realm.'",qop="auth",nonce="'.uniqid().'",opaque="'.md5($realm).'"');
-    die('Incorrect username');
-}
-
-// generate the valid response
-$A1 = md5($data['username'] . ':' . $realm . ':' . $password);
-$A2 = md5($_SERVER['REQUEST_METHOD'].':'.$data['uri']);
-$valid_response = md5($A1.':'.$data['nonce'].':'.$data['nc'].':'.$data['cnonce'].':'.$data['qop'].':'.$A2);
-
-if ($data['response'] != $valid_response){
-    header('HTTP/1.1 401 Unauthorized');
-    header('WWW-Authenticate: Digest realm="'.$realm.'",qop="auth",nonce="'.uniqid().'",opaque="'.md5($realm).'"');
-    die('Incorrect password');
+if( $username ){
+	// function to parse the http auth header
+	function http_digest_parse($txt)
+	{
+		// protect against missing data
+		$needed_parts = array('nonce'=>1, 'nc'=>1, 'cnonce'=>1, 'qop'=>1, 'username'=>1, 'uri'=>1, 'response'=>1);
+		$data = array();
+		$keys = implode('|', array_keys($needed_parts));
+	
+		preg_match_all('@(' . $keys . ')=(?:([\'"])([^\2]+?)\2|([^\s,]+))@', $txt, $matches, PREG_SET_ORDER);
+	
+		foreach ($matches as $m) {
+			$data[$m[1]] = $m[3] ? $m[3] : $m[4];
+			unset($needed_parts[$m[1]]);
+		}
+	
+		return $needed_parts ? false : $data;
+	}
+	
+	if (empty($_SERVER['PHP_AUTH_DIGEST'])) {
+		header('HTTP/1.1 401 Unauthorized');
+		header('WWW-Authenticate: Digest realm="'.$realm.'",qop="auth",nonce="'.uniqid().'",opaque="'.md5($realm).'"');
+		die('login required');
+	}
+	
+	// analyze the PHP_AUTH_DIGEST variable
+	if ( !($data = http_digest_parse($_SERVER['PHP_AUTH_DIGEST'])) || $username !== $data['username'] ){
+		header('HTTP/1.1 401 Unauthorized');
+		header('WWW-Authenticate: Digest realm="'.$realm.'",qop="auth",nonce="'.uniqid().'",opaque="'.md5($realm).'"');
+		die('Incorrect username');
+	}
+	
+	// generate the valid response
+	$A1 = md5($data['username'] . ':' . $realm . ':' . $password);
+	$A2 = md5($_SERVER['REQUEST_METHOD'].':'.$data['uri']);
+	$valid_response = md5($A1.':'.$data['nonce'].':'.$data['nc'].':'.$data['cnonce'].':'.$data['qop'].':'.$A2);
+	
+	if ($data['response'] != $valid_response){
+		header('HTTP/1.1 401 Unauthorized');
+		header('WWW-Authenticate: Digest realm="'.$realm.'",qop="auth",nonce="'.uniqid().'",opaque="'.md5($realm).'"');
+		die('Incorrect password');
+	}
 }
 
 // CORS Allow from shiftedit
 if (isset($_SERVER['HTTP_ORIGIN'])) {
-  header('Access-Control-Allow-Origin: https://shiftedit.net');
+	header('Access-Control-Allow-Origin: https://shiftedit.net');
 	header('Access-Control-Allow-Credentials: true');
 	header('Access-Control-Max-Age: 86400');
 }
@@ -269,7 +272,7 @@ class local{
 		$this->startedAt = time();
 		return $this->search_nodes($s, $path, $file_extensions);
 	}
-substr(decoct( fileperms($entry) ), 2),
+
 	function send_msg($id , $msg) {
 		echo "id: $id" . PHP_EOL;
 		echo "data: {\n";
@@ -485,6 +488,8 @@ switch( $_POST['cmd'] ){
 		}
 
 		$files=array();
+
+		//$_POST['path']=substr($_POST['path'],1);
 
 		if( $_POST['path'] == '/' ){
 			$_POST['path'] = '';
